d1c1b80e2790deeef5ed627cd3bf45e44bc5b562
pv <pedrovgp@gmail.com>
2023-04-12 21:56:31 -0300

    ADD better date format

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -27,6 +27,22 @@
     return isDataLoaded ? props.user.username === currentUser.value.username : false
   })
 
+  function formatDate(date: string | null) {
+    if (!date) {
+      return ''
+    }
+    const d = new Date(date)
+    // add two hours to the date, because the server is in UTC
+    d.setHours(d.getHours() + 2)
+    const year = d.getFullYear()
+    const month = d.getMonth() + 1
+    const day = d.getDate()
+    const hours = d.getHours()
+    const minutes = d.getMinutes()
+    const seconds = d.getSeconds()
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+  }
+
   const isReplying: ComputedRef<boolean> = computed((): boolean => {
     return activeId.value === props.id && actionType.value === REPLYING
   })
@@ -34,7 +50,7 @@
 
 <template>
   <va-card stripe stripe-color="success">
-    <va-card-title>{{ props.user.username }}: {{ createdAt }} </va-card-title>
+    <va-card-title>{{ props.user.username }}: {{ formatDate(createdAt) }} </va-card-title>
     <va-card-content>
       <Content :id="props.id" :content="props.content" />
       <Actions v-if="!isReplying" :id="props.id" :user="props.user" />
